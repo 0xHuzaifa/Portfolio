59a27f241e2b3238a7ed6761a7eb970c579ea9ef
0xhuzaifa <huzaifa.rb00@gmail.com>
2026-04-01 16:07:15 +0500

    configure routes

---
+++ src/config/routes.ts
@@ -0,0 +1,11 @@
+import { ContactPageContent } from "@/components/pages/ContactPageContent";
+import { ExperiencePageContent } from "@/components/pages/ExperiencePageContent";
+import { HomePageContent } from "@/components/pages/HomePageContent";
+import { HowIBuildSystemsPageContent } from "@/components/pages/HowIBuildSystemsPageContent";
+
+export const routes = {
+  "/": HomePageContent,
+  "/contact": ContactPageContent,
+  "/experience": ExperiencePageContent,
+  "/how-i-build-systems": HowIBuildSystemsPageContent,
+};
